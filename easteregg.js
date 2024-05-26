078bc9595b0fd4fa5fc6858f0f40f2a4ec2f48e8
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-05-26 17:37:20 +0900

    Update easteregg.js

--- easteregg.js
+++ easteregg.js
@@ -5,6 +5,7 @@ var allowdate = today.getMonth()+1 + '/' + today.getDate();
 if(allowdate == '5/26'){
   if(url == 'https://naochan83275.github.io/?mode=birthday&ord=13'){
     var msg = url.replace('https://naochan83275.github.io/?mode=birthday&ord=', '');
+    document.getElementById('tabtitle').innerHTML = 'happy birthday,Naochan! | Naochan’s website(easteregg))';
     document.getElementById('msg').innerHTML = '誰か祝ってくれるのかということは置いといてとりあえず' + msg + '歳になりました。<br>※深夜1時に大急ぎで作ったのでミスがあるかもしれません。<br><small>[追記]実際ミスがあり8時に修正しました。</small>';
   }
 }
