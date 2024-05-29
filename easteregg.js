92660950755f4a5bf6815f3a9df5a5b440c527e8
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-05-29 15:37:21 +0900

    Update easteregg.js

--- easteregg.js
+++ easteregg.js
@@ -4,16 +4,20 @@ var today = new Date();
 var ord = today.getFullYear()-2011;
 var allowdate = today.getMonth()+1 + '/' + today.getDate();
 var allowurl = 'https://naochan83275.github.io/?mode=birthday&ord=' + ord;
-//以下のコメント内はデバッグ用
+// 以下デバッグ用
 
 var allowdate = '5/26';
+var ord = 18;
 
+// デバックここまで
 if(allowdate == '5/26'){
-  if(url == 'https://naochan83275.github.io/?mode=birthday&ord=13'){
+  if(url == allowurl){
     var msg = url.replace('https://naochan83275.github.io/?mode=birthday&ord=', '');
     document.getElementById('tabtitle').innerHTML = 'happy birthday,Naochan! | Naochan’s website(easteregg))';
     document.getElementById('msg').innerHTML = '誰か祝ってくれるのかということは置いといてとりあえず' + msg + '歳になりました。<br>※大急ぎで作ったのでミスがあるかもしれません。<br><small>[追記]実際ミスがあり8時に修正しました。</small>';
   }else{
-    document.getElementById('msg').innerHTML = 'urlパラメータ“ord”が間違っているようです。なった年齢をord=の後に指定してください。'
+    if(url.indexOf('https://naochan83275.github.io/?mode=birthday) = -1){
+      document.getElementById('msg').innerHTML = 'urlパラメータ“ord”が間違っているようです。なった年齢をord=の後に指定してください。';
+    }
   }
 }
